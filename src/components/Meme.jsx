bcfdbef47cfbd563ddbef224547ed416ba999e0e
Kel-VinTech <mondaykelvintech@gmail.com>
2022-12-05 20:10:40 -0500

    changes

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 
 const Meme= () => {
+
+
+
+
+
+
     return(
-        <form className="form row">
-                <input placeholder="Top text" className="form-input" type="text"/>    
-                <input placeholder="Bottom text"  className="form-input" type="text"/>
-            <button className="form_btn">Get a new meme image</button>
-        </form>
+
+            <div className="form row">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form-input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form-input"
+                />
+
+                 <button className="form_btn">Get a new meme image</button>
+
+
+            </div>
     )
 }
 
 export default Meme
+
