fdb1c363360233e09fb2d1ae352f53204f533908
Kel-VinTech <mondaykelvintech@gmail.com>
2022-12-09 12:22:47 -0500

    did a challenge on react form

--- src/FormData.js
+++ src/FormData.js
@@ -6,19 +6,32 @@ import Form from "./Form";
 const FormData = () => {
 
     const [FormDatas, setFormDatas] = React.useState(
-        {name:"", email:"", password:"" })
+        {name:"", email:"", password:"",confirm:"" , isFriendly:true})
 
     const handleChange = (e) => {
+        const {name,value,type,checked} = e.target
         setFormDatas(prevFormDatas => {
             return{
                 ...prevFormDatas,
-                [e.target.name]: e.target.value
+                [name]: type === "checkbox" ? checked : value
             }
         })
     }
 
+
     const formSubmit = (e) => {
         e.preventDefault()
+        if(FormDatas.password === FormDatas.confirm) {
+            console.log("Successfully signed up")
+        } else {
+            console.log("password do not match")
+        }
+
+        if(FormDatas.isFriendly === true) {
+            console.log("Thanks for signing up")
+        } else{
+            console.log("Kindly accept newsletter")
+        }
     }
 
     return(
@@ -46,7 +59,25 @@ const FormData = () => {
                 placeholder="Enter password"
                 onChange={handleChange}
                 value={FormDatas.password}
-            />      
+            />
+            <input 
+                type="password"
+                name="confirm"
+                placeholder="Confirm password"
+                onChange={handleChange}
+                value={FormDatas.confirm}
+
+                
+            /> 
+
+            <input 
+                type="checkbox"
+                id="isFriendly"
+                name="isFriendly"
+                onChange={handleChange}
+                checked={FormDatas.isFriendly}
+            />    
+            <label htmlFor="isFriendly">Are you friendly</label>   
 
             <button>Sign up</button>
             
